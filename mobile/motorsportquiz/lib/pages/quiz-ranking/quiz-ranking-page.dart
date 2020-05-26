a52cfe47cdf3cc5f1ae309a516fc650e0e833ee5
Carlos Bonatti <carlos.bonatti@live.com>
2020-05-26 00:16:47 -0300

    Created quiz result animation + making ranking scrollable

--- mobile/motorsportquiz/lib/pages/quiz-ranking/quiz-ranking-page.dart
+++ mobile/motorsportquiz/lib/pages/quiz-ranking/quiz-ranking-page.dart
@@ -35,56 +35,58 @@ class _QuizRankingPageState extends State<QuizRankingPage> {
           )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          FutureBuilder(
-            future: _rankingService.getRanking(),
-            builder: (BuildContext context,
-                AsyncSnapshot<List<QuizRanking>> snapshot) {
-              if (snapshot.hasData) {
-                List<QuizRanking> ranking = snapshot.data;
-                return DataTable(
-                  columns: const <DataColumn>[
-                    DataColumn(
-                      label: Text(
-                        '#',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            FutureBuilder(
+              future: _rankingService.getRanking(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<QuizRanking>> snapshot) {
+                if (snapshot.hasData) {
+                  List<QuizRanking> ranking = snapshot.data;
+                  return DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(
+                        label: Text(
+                          '#',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Nome',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      DataColumn(
+                        label: Text(
+                          'Nome',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
                       ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Potuação',
-                        style: TextStyle(fontStyle: FontStyle.italic),
+                      DataColumn(
+                        label: Text(
+                          'Potuação',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                        ),
                       ),
-                    ),
-                  ],
-                  rows: ranking
-                      .map(
-                        ((element) => DataRow(
-                              cells: <DataCell>[
-                                DataCell(Text(element.position
-                                    .toString())), //Extracting from Map element the value
-                                DataCell(Text(element.name)),
-                                DataCell(Text(
-                                    '${element.result.toStringAsFixed(2)}%')),
-                              ],
-                            )),
-                      )
-                      .toList(),
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
-        ],
+                    ],
+                    rows: ranking
+                        .map(
+                          ((element) => DataRow(
+                                cells: <DataCell>[
+                                  DataCell(Text(element.position
+                                      .toString())), //Extracting from Map element the value
+                                  DataCell(Text(element.name)),
+                                  DataCell(Text(
+                                      '${element.result.toStringAsFixed(2)}%')),
+                                ],
+                              )),
+                        )
+                        .toList(),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- mobile/motorsportquiz/lib/pages/quiz/quiz-result-page.dart
+++ mobile/motorsportquiz/lib/pages/quiz/quiz-result-page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:motorsportquiz/pages/quiz-ranking/models/quiz-ranking.dart';
 import 'package:motorsportquiz/pages/quiz-ranking/quiz-ranking-page.dart';
+import 'package:motorsportquiz/shared/components/circle-progress.dart';
 
 import 'models/question.dart';
 import 'quiz-page.dart';
@@ -14,8 +15,25 @@ class QuizResultPage extends StatefulWidget {
   _QuizResultPageState createState() => _QuizResultPageState();
 }
 
-class _QuizResultPageState extends State<QuizResultPage> {
+class _QuizResultPageState extends State<QuizResultPage>
+    with SingleTickerProviderStateMixin {
   final QuizService _quizService = QuizService();
+  AnimationController progressController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    progressController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    animation = Tween<double>(begin: 0, end: widget.result.result)
+        .animate(progressController)
+          ..addListener(() {
+            setState(() {});
+          });
+
+    progressController.forward();
+  }
 
   void _startQuiz() {
     _quizService.start(widget.result.name).then((value) => {navigate(value)});
@@ -68,7 +86,29 @@ class _QuizResultPageState extends State<QuizResultPage> {
           },
           child: Padding(
               padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-              child: Text('Seu resultado ${widget.result.result}'))),
+              child: Center(
+                  child: Column(
+                children: <Widget>[
+                  Text(
+                    'Seu resultado',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  CustomPaint(
+                    foregroundPainter: CircleProgress(animation.value.toInt()),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      child: GestureDetector(
+                          child: Center(
+                              child: Text(
+                        "${animation.value.toInt()}%",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ))),
+                    ),
+                  ),
+                ],
+              )))),
     );
   }
 }
